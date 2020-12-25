1da18f981a88a65aa54d53a6d3fadc282ae25eee
Shubham Pandey <shubhampandey979271@gmail.com>
2020-12-25 09:49:54 +0530

    Wallpaper App

--- app/src/main/java/papaya/in/mywallpaper/FullImageActivity.java
+++ app/src/main/java/papaya/in/mywallpaper/FullImageActivity.java
@@ -4,25 +4,19 @@ import android.app.WallpaperManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.facebook.ads.Ad;
-import com.facebook.ads.AdError;
-import com.facebook.ads.AdListener;
-import com.facebook.ads.AdSize;
-import com.facebook.ads.AdView;
-import com.facebook.ads.AudienceNetworkAds;
-import com.facebook.ads.InterstitialAd;
-import com.facebook.ads.InterstitialAdListener;
 
 import java.io.IOException;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FullImageActivity extends AppCompatActivity {
@@ -30,11 +24,7 @@ public class FullImageActivity extends AppCompatActivity {
     private ImageView fullImage;
     private Button apply;
 
-    private final String TAG = "FB_ADS";
 
-    private AdView adView;
-
-    private InterstitialAd interstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,97 +34,6 @@ public class FullImageActivity extends AppCompatActivity {
         fullImage = findViewById(R.id.fullImage);
         apply = findViewById(R.id.apply);
 
-        // Initialize the Audience Network SDK
-        AudienceNetworkAds.initialize(this);
-
-        adView = new AdView(this, "1508280382690917_1508286636023625", AdSize.BANNER_HEIGHT_50);
-
-        // Find the Ad Container
-        LinearLayout adContainer = (LinearLayout) findViewById(R.id.banner_container);
-
-        // Add the ad view to your activity layout
-        adContainer.addView(adView);
-
-        AdListener adListener = new AdListener() {
-            @Override
-            public void onError(Ad ad, AdError adError) {
-                // Ad error callback
-                Toast.makeText(
-                        FullImageActivity.this,
-                        "Error: " + adError.getErrorMessage(),
-                        Toast.LENGTH_LONG)
-                        .show();
-            }
-
-            @Override
-            public void onAdLoaded(Ad ad) {
-                // Ad loaded callback
-            }
-
-            @Override
-            public void onAdClicked(Ad ad) {
-                // Ad clicked callback
-            }
-
-            @Override
-            public void onLoggingImpression(Ad ad) {
-                // Ad impression logged callback
-            }
-        };
-
-        // Request an ad
-        adView.loadAd(adView.buildLoadAdConfig().withAdListener(adListener).build());
-
-
-        interstitialAd = new InterstitialAd(this, "1508280382690917_1508292322689723");
-
-
-        InterstitialAdListener interstitialAdListener = new InterstitialAdListener() {
-            @Override
-            public void onInterstitialDisplayed(Ad ad) {
-                // Interstitial ad displayed callback
-                Log.e(TAG, "Interstitial ad displayed.");
-            }
-
-            @Override
-            public void onInterstitialDismissed(Ad ad) {
-                // Interstitial dismissed callback
-                Log.e(TAG, "Interstitial ad dismissed.");
-            }
-
-            @Override
-            public void onError(Ad ad, AdError adError) {
-                // Ad error callback
-                Log.e(TAG, "Interstitial ad failed to load: " + adError.getErrorMessage());
-            }
-
-            @Override
-            public void onAdLoaded(Ad ad) {
-                // Interstitial ad is loaded and ready to be displayed
-                Log.d(TAG, "Interstitial ad is loaded and ready to be displayed!");
-                // Show the ad
-                interstitialAd.show();
-            }
-
-            @Override
-            public void onAdClicked(Ad ad) {
-                // Ad clicked callback
-                Log.d(TAG, "Interstitial ad clicked!");
-            }
-
-            @Override
-            public void onLoggingImpression(Ad ad) {
-                // Ad impression logged callback
-                Log.d(TAG, "Interstitial ad impression logged!");
-            }
-        };
-
-        // For auto play video ads, it's recommended to load the ad
-        // at least 30 seconds before it is shown
-        interstitialAd.loadAd(
-                interstitialAd.buildLoadAdConfig()
-                        .withAdListener(interstitialAdListener)
-                        .build());
 
         Glide.with(this).load(getIntent().getStringExtra("image")).into(fullImage);
 
@@ -144,14 +43,6 @@ public class FullImageActivity extends AppCompatActivity {
                 setBackground();
             }
         });
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (adView != null){
-            adView.destroy();
-        }
     }
 
     private void setBackground() {
